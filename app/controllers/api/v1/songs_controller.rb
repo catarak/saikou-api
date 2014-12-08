55bac8c56a8677648c5060372bc2ac59fedc3776
catarak <ctarakajian@gmail.com>
2014-12-07 22:18:56 -0500

    add 404 response to songs#index

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -1,8 +1,16 @@
 class Api::V1::SongsController < ApplicationController
   def index
     @year = Year.find_by(number: params[:year_number].to_i)
-    @week = Week.find_by(number: params[:week_number].to_i, year_id: @year.id)
-    @records = Record.where(week_id: @week.id)
-    @countries = Country.joins(charts: :records).where(records: {week_id: @week.id})
+    if @year.nil?
+      render json: {message: 'Resource not found'}, status: 404
+    else
+      @week = Week.find_by(number: params[:week_number].to_i, year_id: @year.id)
+      if @week.nil? 
+        render json: {message: 'Resource not found'}, status: 404
+      else
+        @records = Record.where(week_id: @week.id)
+        @countries = Country.joins(charts: :records).where(records: {week_id: @week.id})
+      end
+    end
   end
 end
